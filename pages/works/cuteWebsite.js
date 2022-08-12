a59063c074d977da53765ab1a9ce6c13c50998aa
Вася Бураев <buraevvasilii@gmail.com>
2022-08-12 15:11:14 +0300

    final change link

--- pages/works/cuteWebsite.js
+++ pages/works/cuteWebsite.js
@@ -16,7 +16,7 @@ const Work = () => (
       <List ml={4} my={4}>
         <ListItem>
           <Meta>Git</Meta>
-          <Link href="https://github.com/Prince-Gizard/buraev-website.git">
+          <Link href="https://github.com/Prince-Gizard/my-homepage">
             https://github.com/Prince-Gizard/my-homepage <ExternalLinkIcon mx="2px" />
           </Link>
         </ListItem>
