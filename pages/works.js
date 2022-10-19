61302ebb5686064143be7f072de5a8d1886246fe
Prince-Gizard <buraevvasilii@gmail.com>
2022-10-19 13:26:58 +0300

    simple edit

--- pages/works.js
+++ pages/works.js
@@ -17,29 +17,8 @@ const Works = () => {
       <Heading as='h3' fountSize={20} mb={4}>
        Works
       </Heading>
-
-    <SimpleGrid columns={[1, 1, 2]} gap={6}>
-      <Section delay={0.1}>
-       <WorkGridItem id="cuteWebsite" title="Cute Website" thumbnail={cuteWebsite}>
-          My personal website inspired by TM
-        </WorkGridItem>
-      </Section>
-      <Section delay={0.1}>
-        <WorkGridItem id="blog" title="My personal blog" thumbnail={blog}>
-          My personal blog whis admin panel
-        </WorkGridItem>
-      </Section>
-    </SimpleGrid>
-      
-    <Section delay={0.2}>
-      <Divider my={6} />
-      
-      <Heading as='h3' fontSize={20} mb={4}>
-        Project
-      </Heading>
-    </Section>
-
-    <SimpleGrid columns={[1,1,2]} gap={6}>
+    
+      <SimpleGrid columns={[1,1,2]} gap={6}>
        <Section delay={0.1}>
         <WorkGridItem id="cryptoTest" title="Crypto transaction" thumbnail={cryptoTest}>
           Project whith blockchain technology
@@ -47,26 +26,45 @@ const Works = () => {
       </Section>
 
       <Section delay={0.1}>
-        <WorkGridItem id="dioBrando" title="Dio terminal" thumbnail={dioBrando}>
-          Linux terminal website
+        <WorkGridItem id="dioBrando" title="Terminal style website" thumbnail={dioBrando}>
+          Build fast terminal styled websites 
         </WorkGridItem>
       </Section>
+  
+       <Section delay={0.1}>
+        <WorkGridItem id="mamaPhoto" title="Project for photographer" thumbnail={mamaPhoto}>
+          Site for children photographer 
+        </WorkGridItem>
+       </Section>
+     </SimpleGrid>
+  
+    <Section delay={0.2}>
+      <Divider my={6} />
+      <Heading as='h3' fontSize={20} mb={4}>
+        Personal project
+      </Heading>
+    </Section>
     
+    <SimpleGrid columns={[1, 1, 2]} gap={6}>
       <Section delay={0.1}>
+       <WorkGridItem id="cuteWebsite" title="Cute Website" thumbnail={cuteWebsite}>
+          My personal website inspired by TM
+        </WorkGridItem>
+      </Section>
+
+       <Section delay={0.1}>
         <WorkGridItem id="whatUses" title="What I Use" thumbnail={whatUses}>
           List of tech i use
         </WorkGridItem>
-      </Section>
-      
+        </Section>
+
       <Section delay={0.1}>
-        <WorkGridItem id="mamaPhoto" title="Site for Photographer" thumbnail={mamaPhoto}>
-          Site for children photographer 
+        <WorkGridItem id="blog" title="My personal blog" thumbnail={blog}>
+          My personal blog whis admin panel
         </WorkGridItem>
       </Section>
-
-
-    </SimpleGrid>
-
+    
+     </SimpleGrid>
     </Container>
     </Layout>
   )
